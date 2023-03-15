c86b6a80141113619d5e17eba6cbb6cd36da9933
Mateus Souza <61070878+MatteusSouza@users.noreply.github.com>
2023-03-15 03:47:44 -0300

    implemented points system with coin collections

---
+++ Assets/scripts/collectCoins.cs
@@ -0,0 +1,29 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collectCoins : MonoBehaviour
+{
+    public Text scoreTxt;
+    private int score;
+    private void Start()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        scoreTxt.text = score.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("coin") == true)
+        {
+            score = score + 1;
+            Destroy(col.gameObject);
+        }
+    }
+}
